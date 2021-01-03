06e11438769da256ae21ee633c8e4ba247c073f1
Amomanu <75492065+Amomanu@users.noreply.github.com>
2021-01-03 17:20:51 -0500

    Updates for Jims Tenant
    Updated tenant id and subId in runtf.sh

--- aksv2k8TF/runtf.sh
+++ aksv2k8TF/runtf.sh
@@ -3,8 +3,8 @@ read -p "Enter keyvault name: " env_key_vault_name
 sed -i "s/kvnames/$env_key_vault_name/" "akvs-secret-sync.yaml"
 sed -i "s/kvnames/$env_key_vault_name/" "akvs-secret-inject.yaml"
 
-az login --tenant 3ce02252-982a-4d82-ada3-a7a8d9617394
-az account set --subscription=f297f3ca-3530-4fe1-b9b5-fda6f21aec1d
+az login --tenant 9917dcc8-bdaf-4e03-928b-1e67b0d806c5 
+az account set --subscription=a2d2880c-29d4-407f-bd42-08d4832d925e
 
 terraform init
 terraform apply --var key_vault_name="${env_key_vault_name}"
